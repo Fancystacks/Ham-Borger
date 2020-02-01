0a709776b00d5eb08a8dc9662dd8a02e3af0c5a0
Leah Migacz <lmigacz@msudenver.edu>
2020-01-31 20:32:30 -0700

    added js starting code

---
+++ public/js/burger.js
@@ -0,0 +1,57 @@
+$(function() {
+
+    $(".delete-cat").on("click", function() {
+      const id = $("this").attr("data-id");
+      // delete selected cat
+      $.ajax(`/api/cats/${id}`, {
+        type: "DELETE"
+      }).then(() => {
+        console.log(`Deleted cat with id: ${id}`);
+        location.reload();
+      });
+    });
+  
+    $(".change-sleep").on("click", function(event) {
+      var id = $(this).data("id");
+      var newSleep = $(this).data("newsleep");
+  
+      var newSleepState = {
+        sleepy: newSleep
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/cats/" + id, {
+        type: "PUT",
+        data: newSleepState
+      }).then(
+        function() {
+          console.log("changed sleep to", newSleep);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    // { name: sleepy }
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newCat = {
+        name: $("#ca").val().trim(),
+        sleepy: $("[name=sleepy]:checked").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/cats", {
+        type: "POST",
+        data: newCat
+      }).then(
+        function() {
+          console.log("created new cat");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
