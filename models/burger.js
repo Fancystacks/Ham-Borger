85a3d81d06ebc4a10423106ad7b98758cd406f1a
Leah Migacz <lmigacz@msudenver.edu>
2020-01-31 21:09:17 -0700

    burger model is completely done and updated

---
+++ models/burger.js
@@ -0,0 +1,29 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var burgers = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  create: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+  delete: function(condition, cb) {
+    // arm.delete(tableName, db condition, cb)
+    orm.delete("burgers", condition, res => {
+    cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (burgers_llerontr.js).
+module.exports = burgey;
