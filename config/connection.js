fe57c3dd03202186dee16ab98822c9e9d4ec8a6c
Leah Migacz <lmigacz@msudenver.edu>
2020-02-04 08:37:23 -0700

    deploying to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 var dotenv = require("dotenv").config();
+var connection;
 
-var connection = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  port: 3306,
-  database: process.env.DATABASE
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+      host: process.env.HOST
+      , user: process.env.USERNAME
+      , password: process.env.PASSWORD
+      , port: 3306
+      , database: process.env.DATABASE
+  });
 
+};
 // Make connection.
 connection.connect(function(err) {
   if (err) {
